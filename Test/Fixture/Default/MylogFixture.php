e3be606a5c956032b3f8f663f5986e60941c5d38
hideichi saito <binbin4649@gmail.com>
2021-04-11 16:42:16 +0900

    test fixture fix.

--- Test/Fixture/Default/MylogFixture.php
+++ Test/Fixture/Default/MylogFixture.php
@@ -9,7 +9,7 @@ class MylogFixture extends CakeTestFixture {
 			[
 				'id' => 1,
 				'mypage_id' => 1,
-				'user_id' => '',
+				'user_id' => null,
 				'action' => 'message_submit',
 				'history' => 'テスト1だよーん',
 				'created' => '2018-07-30 14:06:01',
@@ -18,7 +18,7 @@ class MylogFixture extends CakeTestFixture {
 			[
 				'id' => 2,
 				'mypage_id' => 2,
-				'user_id' => '',
+				'user_id' => null,
 				'action' => 'signin',
 				'history' => '',
 				'created' => date('Y-m-d H:i:s', strtotime('-10 day')),
@@ -27,7 +27,7 @@ class MylogFixture extends CakeTestFixture {
 			[
 				'id' => 3,
 				'mypage_id' => 2,
-				'user_id' => '',
+				'user_id' => null,
 				'action' => 'signin',
 				'history' => '',
 				'created' => date('Y-m-d H:i:s', strtotime('-60 day')),
